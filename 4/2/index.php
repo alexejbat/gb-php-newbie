753ed13ad9ee0d529e2fcd22e9c9cd7609a6405d
alexejbat <alexejbat@yandex.ru>
2022-02-28 15:16:12 +0400

    add lesson 1
    2 - 5 hw

---
+++ 4/2/index.php
@@ -0,0 +1,9 @@
+<?php
+const IMG_DIR = 'img/';
+
+$title = "Главная страница - страница обо мне";
+$h1 = "Информация обо мне";
+$year = date('Y');
+$images = "1.jpg";
+
+include "templates/main.php";
