1d76fa72f0e4462b5e478469bbd4b0a8d4cc0e11
alexejbat <alexejbat@yandex.ru>
2022-03-06 12:44:58 +0400

    add upload php
    div upload gallery php

--- templates/gallery.php
+++ templates/gallery.php
@@ -3,3 +3,12 @@
         <a href="/public/gallery_img/big/<?=$filename?>"><img src="/public/gallery_img/small/<?=$filename?>"></a>
     <? endforeach; ?>
 </div>
+<br><br>
+<br>
+<div id="upload">
+    <br>
+    <form action="../engine/upload.php" method="post" enctype="multipart/form-data">
+        <p><input type="file" name="userfile" id="file"></p>
+        <p><input type="submit" value="Загрузить фаил"></p>
+    </form>
+</div>
