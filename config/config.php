14b7d6c1c591479649b7f74d51472988fc8fa21f
alexejbat <alexejbat@yandex.ru>
2022-03-25 11:12:28 +0400

    lesson-5
    add files

---
+++ config/config.php
@@ -0,0 +1,20 @@
+<?php
+
+define("ROOT", dirname(__DIR__));
+define("IMG_BIG", $_SERVER['DOCUMENT_ROOT'] . "/gallery_img/big/");
+define("IMG_SMALL", $_SERVER['DOCUMENT_ROOT'] . "/gallery_img/small/");
+define('TEMPLATES_DIR', ROOT . '/templates/');
+define('LAYOUTS_DIR', 'layouts/');
+
+/* DB config */
+define('HOST', 'localhost:3306');
+define('USER', 'root');
+define('PASS', '');
+define('DB', 'gb_student');
+
+include ROOT . "/engine/functions.php";
+include ROOT . "/engine/db.php";
+include ROOT . "/engine/log.php";
+include ROOT . "/engine/gallery.php";
+include ROOT . "/engine/news.php";
+include ROOT . "/engine/classSimpleImage.php";
---
+++ templates/layouts/layout.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Интернет-магазин</title>
+    <link rel="stylesheet" type="text/css" href="/css/style.css"/>
+</head>
+    <?=$menu?>
+    <?=$content?>
+</body>
+</html>
