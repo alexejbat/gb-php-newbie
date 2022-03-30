c2fc1d6012970e679a2a53039971bd294c5b3d51
alexejbat <alexejbat@yandex.ru>
2022-03-30 09:22:31 +0400

    lesson-6
    update

--- templates/feedback.php
+++ templates/feedback.php
@@ -3,18 +3,18 @@
 <?=$message?>
 
 <form action="/feedback/<?=$action?>/" method="post">
-    <p>Оставьте ваш отзыв:</p><br>
+    Оставьте отзыв: <br>
     <input hidden type="text" name="id_feedback" value="<?=$id_feed?>"><br>
-    <input type="text" name="name" placeholder="Ваше ФИО" value="<?=$name?>"><br>
-    <input type="text" name="feedback" placeholder="Ваш отзыв" value="<?=$feedback?>"><br>
-    <input type="submit" value="<?=$buttonText?>"><br>
+    <input type="text" name="name" placeholder="Ваше имя" value="<?=$name?>"><br>
+    <input type="text" name="feedback" placeholder="Ваш отзыв" value="<?=$text?>"><br>
+    <input type="submit" value="<?=$button?>">
 </form>
 
 <?php foreach ($feedback as $value): ?>
 
-    <div style="..."><strong><?=$value['name']?></strong>: <?=$value['feedback']?>
-        <a href="/feedback/edit/?id=<?=$value['id']?>">[Править]</a>
-        <a href="/feedback/?delete=<?=$value['id']?>">[Удалить]</a>
+    <div style="margin-top: 10px"><strong><?=$value['name']?></strong>: <?=$value['feedback']?>
+        <a href="/feedback/edit/?id_feed=<?=$value['id']?>">[править]</a>
+        <a href="/feedback/delete/?id_feed=<?=$value['id']?>">[X]</a>
     </div>
 
 <?endforeach;?>
