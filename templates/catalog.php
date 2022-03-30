eaa4fd19f2f6aa09707489307baba475bbc136ab
alexejbat <alexejbat@yandex.ru>
2022-03-30 16:25:34 +0400

    lesson-6
    update

--- templates/catalog.php
+++ templates/catalog.php
@@ -4,8 +4,8 @@
 <div class="card">
     <a href="/catalog_item/?id=<?=$item['id']?>">
         <h2><?=$item["name"]?></h2>
-    <a href="/catalog_item/<?=$item['id']?>"><?=$item["description"]?></a>
-    <a href="/catalog_item/<?= $item['id'] ?>"><img src="/catalog_img/<?= $item['image']?>" alt="item" width="263px" ></a><br>
+    <a href="/catalog_item/?id=<?=$item['id']?>"><?=$item["description"]?></a>
+    <a href="/catalog_item/?id=<?=$item['id']?>"><img src="/catalog_img/<?= $item['image']?>" alt="item" width="263px" ></a><br>
 
     Стоимость: <?=$item["price"]?><br>
     <button>Купить</button><hr>
